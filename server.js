9b6701f8e0098195d97b3bf4792873d469931cb6
Farhad Mirzapour <131616231+farhadpage@users.noreply.github.com>
2023-04-23 23:33:02 +0330

    Create server.js

---
+++ server.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const TelegramBot = require("node-telegram-bot-api");
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_TOKEN || "";
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf(token);
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+
+
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+    })
+});
+
+
+
+
+//method that displays the inline keyboard buttons 
+
+bot.hears('animals', ctx => {
+    console.log(ctx.from)
+    let animalMessage = `great, here are pictures of animals you would love`;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "dog",
+                        callback_data: 'dog'
+                    },
+                    {
+                        text: "cat",
+                        callback_data: 'cat'
+                    }
+                ],
+
+            ]
+        }
+    })
+})
+
+//method that returns image of a dog
+
+bot.action('dog', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, 'doooooooooooooooog', {
+    })
+})
+
+//method that returns image of a cat 
+
+bot.action('cat', ctx => {
+     bot.telegram.sendMessage(ctx.chat.id, 'caaaaaaaaaaaaaaaat', {
+	})
+})
+
+
+
+
+//method for requesting user's phone number
+
+ bot.hears('phone', (ctx, next) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
+
+})
+
+//method for requesting user's location
+
+bot.hears("location", (ctx) => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
+})
+
+//constructor for providing phone number to the bot
+
+const requestPhoneKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My phone number",
+                request_contact: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+};
+//constructor for proving location to the bot
+
+const requestLocationKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "My location",
+                request_location: true,
+                one_time_keyboard: true
+            }],
+            ["Cancel"]
+        ]
+    }
+
+}
+
+
+
+const app = express();
+app.use(express.static('static'));
+app.use(express.json());  
+app.use(bot.webhookCallback('/secret-path'));
+bot.telegram.setWebhook('https://nice-plum-sheep.cyclic.app/secret-path');
+
+app.listen(process.env.PORT || 3000);
